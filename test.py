ca9c7d98f1c585f163b7a1df35f56647b44b4d85
Luyaban <115881662+Luyaban@users.noreply.github.com>
2023-04-01 20:12:18 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,134 +1,56 @@
-# This program allows a user to enter a
-# Code. If the C-Button is pressed on the
-# keypad, the input is reset. If the user
-# hits the A-Button, the input is checked.
-
+test_keypad_polling.py
 import RPi.GPIO as GPIO
 import time
 
-# These are the GPIO pin numbers where the
-# lines of the keypad matrix are connected
-L1 = 5
-L2 = 6
-L3 = 13
-L4 = 19
+# Set the Row Pins
+ROW_1 = 17
+ROW_2 = 27
+ROW_3 = 22
+ROW_4 = 5
 
-# These are the four columns
-C1 = 12
-C2 = 16
-C3 = 20
-C4 = 21
+# Set the Column Pins
+COL_1 = 23
+COL_2 = 24
+COL_3 = 25
+COL_4 = 16
 
-# The GPIO pin of the column of the key that is currently
-# being held down or -1 if no key is pressed
-keypadPressed = -1
-
-secretCode = "4789"
-input = ""
-
-# Setup GPIO
 GPIO.setwarnings(False)
+# BCM numbering
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(L1, GPIO.OUT)
-GPIO.setup(L2, GPIO.OUT)
-GPIO.setup(L3, GPIO.OUT)
-GPIO.setup(L4, GPIO.OUT)
+# Set Row pins as output
+GPIO.setup(ROW_1, GPIO.OUT)
+GPIO.setup(ROW_2, GPIO.OUT)
+GPIO.setup(ROW_3, GPIO.OUT)
+GPIO.setup(ROW_4, GPIO.OUT)
 
-# Use the internal pull-down resistors
-GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Set column pins as input and Pulled up high by default
+GPIO.setup(COL_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(COL_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(COL_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(COL_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# This callback registers the key that was pressed
-# if no other key is currently pressed
-def keypadCallback(channel):
-    global keypadPressed
-    if keypadPressed == -1:
-        keypadPressed = channel
-
-# Detect the rising edges on the column lines of the
-# keypad. This way, we can detect if the user presses
-# a button when we send a pulse.
-GPIO.add_event_detect(C1, GPIO.RISING, callback=keypadCallback)
-GPIO.add_event_detect(C2, GPIO.RISING, callback=keypadCallback)
-GPIO.add_event_detect(C3, GPIO.RISING, callback=keypadCallback)
-GPIO.add_event_detect(C4, GPIO.RISING, callback=keypadCallback)
-
-# Sets all lines to a specific state. This is a helper
-# for detecting when the user releases a button
-def setAllLines(state):
-    GPIO.output(L1, state)
-    GPIO.output(L2, state)
-    GPIO.output(L3, state)
-    GPIO.output(L4, state)
-
-def checkSpecialKeys():
-    global input
-    pressed = False
-
-    GPIO.output(L3, GPIO.HIGH)
-
-    if (GPIO.input(C4) == 1):
-        print("Input reset!");
-        pressed = True
-
-    GPIO.output(L3, GPIO.LOW)
-    GPIO.output(L1, GPIO.HIGH)
-
-    if (not pressed and GPIO.input(C4) == 1):
-        if input == secretCode:
-            print("Code correct!")
-            # TODO: Unlock a door, turn a light on, etc.
-        else:
-            print("Incorrect code!")
-            # TODO: Sound an alarm, send an email, etc.
-        pressed = True
-
-    GPIO.output(L3, GPIO.LOW)
-
-    if pressed:
-        input = ""
-
-    return pressed
-
-# reads the columns and appends the value, that corresponds
-# to the button, to a variable
-def readLine(line, characters):
-    global input
-    # We have to send a pulse on each line to
-    # detect button presses
-    GPIO.output(line, GPIO.HIGH)
-    if(GPIO.input(C1) == 1):
-        input = input + characters[0]
-    if(GPIO.input(C2) == 1):
-        input = input + characters[1]
-    if(GPIO.input(C3) == 1):
-        input = input + characters[2]
-    if(GPIO.input(C4) == 1):
-        input = input + characters[3]
+# function to read each row and each column
+def readRow(line, characters):
     GPIO.output(line, GPIO.LOW)
+    if(GPIO.input(COL_1) == GPIO.LOW):
+        print(characters[0])
+    if(GPIO.input(COL_2) == GPIO.LOW):
+        print(characters[1])
+    if(GPIO.input(COL_3) == GPIO.LOW):
+        print(characters[2])
+    if(GPIO.input(COL_4) == GPIO.LOW):
+        print(characters[3])
+    GPIO.output(line, GPIO.HIGH)
 
+# Endless loop by checking each row 
 try:
     while True:
-        # If a button was previously pressed,
-        # check, whether the user has released it yet
-        if keypadPressed != -1:
-            setAllLines(GPIO.HIGH)
-            if GPIO.input(keypadPressed) == 0:
-                keypadPressed = -1
-            else:
-                time.sleep(0.1)
-        # Otherwise, just read the input
-        else:
-            if not checkSpecialKeys():
-                readLine(L1, ["1","2","3","A"])
-                readLine(L2, ["4","5","6","B"])
-                readLine(L3, ["7","8","9","C"])
-                readLine(L4, ["*","0","#","D"])
-                time.sleep(0.1)
-            else:
-                time.sleep(0.1)
+        readRow(ROW_1, ["1","2","3","A"])
+        readRow(ROW_2, ["4","5","6","B"])
+        readRow(ROW_3, ["7","8","9","C"])
+        readRow(ROW_4, ["*","0","#","D"])
+        time.sleep(0.2) # adjust this per your own setup
 except KeyboardInterrupt:
-    print("\nApplication stopped!")
+    print("\nKeypad Application Interrupted!")
+    GPIO.cleanup()
